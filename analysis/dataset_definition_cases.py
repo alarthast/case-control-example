5a9e8168604790e6cfba8fa18b59b8870a49f971
Alice Wong <146962159+alarthast@users.noreply.github.com>
2025-05-21 17:52:00 +0000

    Add columns to check logic
    There is no need for these to go into the actual dataset definition

--- analysis/dataset_definition_cases.py
+++ analysis/dataset_definition_cases.py
@@ -13,8 +13,11 @@ dataset.define_population(is_diabetic & programme_attendances.exists_for_patient
 dataset.diagnosis_date = first_diagnosis.date.to_first_of_month()
 dataset.age = patients.age_on(first_diagnosis.date)
 dataset.sex = patients.sex
-
 dataset.case_index_date = first_programme_attendance.date
+
+dataset.resolved_date = (
+    resolved_events.sort_by(resolved_events.date).first_for_patient().date
+)
 dataset.has_resolution_event = resolved_events.where(  # Parallels has_event_in_codelist
     resolved_events.date.is_on_or_after(first_programme_attendance.date)
 ).exists_for_patient()
--- analysis/dataset_definition_controls.py
+++ analysis/dataset_definition_controls.py
@@ -23,7 +23,11 @@ dataset.define_population(matched_patients.exists_for_patient())
 dataset.diagnosis_date = first_diagnosis.date.to_first_of_month()
 dataset.age = patients.age_on(first_diagnosis.date)
 dataset.sex = patients.sex
+dataset.case_index_date = matched_patients.case_index_date
 
+dataset.resolved_date = (
+    resolved_events.sort_by(resolved_events.date).first_for_patient().date
+)
 dataset.has_resolution_event = resolved_events.where(
     resolved_events.date.is_on_or_after(matched_patients.case_index_date)
 ).exists_for_patient()
